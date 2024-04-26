61d7f74704a7091dbf4662ebbc6461869df58bdc
Maggy <edoziemagdalene@gmail.com>
2024-04-26 09:43:15 +0100

    adding sentry

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import { SafeAreaView, StatusBar } from "react-native";
 
 import { MainNav } from "./src/navigation/MainNav";
+import * as Sentry from "@sentry/react-native";
 
-export default function App() {
+Sentry.init({
+  dsn: "https://d2f24b86e8003303be164a5da008d4d9@o4507038565662720.ingest.us.sentry.io/4507147909267456",
+});
+
+function App() {
   return (
     <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
       <MainNav />
@@ -10,3 +15,5 @@ export default function App() {
     </SafeAreaView>
   );
 }
+
+export default Sentry.wrap(App);
